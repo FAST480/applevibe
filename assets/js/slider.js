52a6bc3be7212b55d2ec3336bec7c72681845707
FAST480 <61682629+FAST480@users.noreply.github.com>
2023-10-30 19:24:55 +0300

    Add files via upload

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,13 +1,26 @@
 const sliderScrollBlock = document.querySelector(".main__events__list");
 
 const sliderItems = document.querySelectorAll(".main__events__item");
+const dotsItems = document.querySelectorAll(".main__events__dots__item");
 
 let currentSlide = 0;
 
 let itemWidth = document.querySelector(".main__events__item").offsetWidth;
 
-function getPosition(){
+function getPosition(currentSlide){
 	return currentSlide*(-1)*itemWidth;
+}
+
+function disactivateAllDots()
+{
+	dotsItems.forEach((dot)=>{
+		dot.classList.remove("active");
+	});
+}
+
+function setDot(currentSlide){
+	disactivateAllDots();
+	dotsItems[currentSlide].classList.add("active");
 }
 
 
@@ -15,12 +28,24 @@ function nextSlide(){
 	if(currentSlide < sliderItems.length - 1)
 	{
 		currentSlide++;
-		sliderScrollBlock.style.transform = "translateX("+getPosition()+"px)";
+		sliderScrollBlock.style.transform = "translateX("+getPosition(currentSlide)+"px)";
+		setDot(currentSlide);
 	} else if(currentSlide == sliderItems.length - 1)
 	{
 		currentSlide = 0;
-		sliderScrollBlock.style.transform = "translateX("+getPosition()+"px)";
+		sliderScrollBlock.style.transform = "translateX("+getPosition(currentSlide)+"px)";
+		setDot(currentSlide);
 	}
 }
+
+for(let i = 0; i < dotsItems.length; i++)
+{
+	dotsItems[i].addEventListener("click", ()=>{
+		currentSlide = i;
+		sliderScrollBlock.style.transform = "translateX("+getPosition(currentSlide)+"px)";
+		setDot(currentSlide);
+	});
+}
+
 
 let sliderTimer = setInterval(nextSlide, 6000);
