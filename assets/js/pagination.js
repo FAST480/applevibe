b36fbfc4988733913f667602c8c775cafdc14b47
FAST480 <61682629+FAST480@users.noreply.github.com>
2023-10-31 21:31:18 +0300

    Add files via upload

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -1,86 +1,94 @@
-const newsBlock = document.querySelector(".newsMain__list");
-const newsItems = document.querySelectorAll(".newsMain__item");
+window.onload = () => {
+
+	const newsBlock = document.querySelector(".newsMain__list");
+	const newsItems = document.querySelectorAll(".newsMain__item");
 
 
-const paginatorBlock = document.querySelector(".newsMain__paginator");
-const paginatorItem = document.querySelector(".newsMain__paginator__item");
+	const paginatorBlock = document.querySelector(".newsMain__paginator");
+	const paginatorItem = document.querySelector(".newsMain__paginator__item");
 
 
-const maxOnPage = 3;
-let currentPage = 1;
-let blockHeight = (newsItems[0].offsetHeight+50)*maxOnPage;
+	const maxOnPage = 3;
+	let currentPage = 1;
+	let blockHeight = (newsItems[0].offsetHeight+50)*maxOnPage;
+	console.log(newsItems[0].scrollHeight);
 
-function startPaginator()
-{
-	createPaginatorItems();
-	newsBlock.style.maxHeight = blockHeight + "px"
-	newsBlock.style.transform = "translateY(0px)";
-}
-
-
-function getPosition(page)
-{
-	return (page-1)*blockHeight;
-}
-
-function toPage(page)
-{
-
-	newsBlock.style.transform = "translateY(-"+getPosition(page)+"px)";
-
-	currentPage = page;
-
-	const topOffset = document.querySelector('.header').offsetHeight;
-    const elementPosition = newsBlock.parentNode.getBoundingClientRect().top;
-    const offsetPosition = elementPosition - topOffset;
-
-    window.scrollBy({
-        top: offsetPosition,
-        behavior: 'smooth'
-    });
-
-	setActivePaginatorItem();
-}
-
-
-
-function createPaginatorItems()
-{
-	let pages = Math.ceil(newsItems.length / maxOnPage);
-	if(pages > 1)
+	function startPaginator()
 	{
-		for(let i = 1; i < pages; i++)
+		createPaginatorItems();
+		newsBlock.style.maxHeight = blockHeight + "px"
+		newsBlock.style.transform = "translateY(0px)";
+	}
+
+
+	function getPosition(page)
+	{
+		return (page-1)*blockHeight;
+	}
+
+	function toPage(page)
+	{
+
+		newsBlock.style.transform = "translateY(-"+getPosition(page)+"px)";
+
+		currentPage = page;
+
+		const topOffset = document.querySelector('.header').offsetHeight;
+	    const elementPosition = newsBlock.parentNode.getBoundingClientRect().top;
+	    const offsetPosition = elementPosition - topOffset;
+
+	    window.scrollBy({
+	        top: offsetPosition,
+	        behavior: 'smooth'
+	    });
+
+		setActivePaginatorItem();
+	}
+
+
+
+	function createPaginatorItems()
+	{
+		let pages = Math.ceil(newsItems.length / maxOnPage);
+		if(pages > 1)
 		{
-			let clone = paginatorItem.cloneNode(true);
-			clone.textContent = i+1;
-			clone.classList.remove("active");
-			paginatorBlock.appendChild(clone);
+			for(let i = 1; i < pages; i++)
+			{
+				let clone = paginatorItem.cloneNode(true);
+				clone.textContent = i+1;
+				clone.classList.remove("active");
+				paginatorBlock.appendChild(clone);
+			}
 		}
 	}
-}
 
-startPaginator();
+	startPaginator();
 
 
-const paginatorItems = document.querySelectorAll(".newsMain__paginator__item");
-paginatorItems.forEach((item)=>{
-	item.addEventListener("click", (e)=>{
-		e.preventDefault();
-
-		let page = item.textContent;
-		if(page != currentPage) toPage(page);
-		
-	});
-});
-
-function setActivePaginatorItem()
-{
+	const paginatorItems = document.querySelectorAll(".newsMain__paginator__item");
 	paginatorItems.forEach((item)=>{
+		item.addEventListener("click", (e)=>{
+			e.preventDefault();
 
-		item.classList.remove("active");
-
+			let page = item.textContent;
+			if(page != currentPage) toPage(page);
+			
+		});
 	});
 
-	paginatorItems[currentPage-1].classList.add("active");
+	function setActivePaginatorItem()
+	{
+		paginatorItems.forEach((item)=>{
+
+			item.classList.remove("active");
+
+		});
+
+		paginatorItems[currentPage-1].classList.add("active");
+	}
+
+
+
 }
+
 
